2476c2f5a00d545b05f7c4916dac195eedbb8678
Ogunbo Temilola <temmyogunbo@gmail.com>
2017-04-07 17:04:44 +0100

    included code

--- app/library.js
+++ app/library.js
@@ -4,25 +4,30 @@ module.exports = {
 
  /* Find the average of three integers */
  computeAverage: function(num1, num2, num3) {
-   var average;
+   var average=(num1 + num2 +num3)/3;
    return average;
  },
  
  /* Find the factorial of an integer */
  computeFactorial: function(num) {
-   var factorial;
+   var factorial=1;
+   for(var i=num; i>0; i--){
+   	factorial *=i
+   }
    return factorial;
  },
 
  /* Convert a given Celcius temperature to Fahrenheit */
  convertTempCtoF: function(cTemp) {
    var fTemp;
+   fTemp = ((9/5)*cTemp)+32;
    return fTemp;
  },
 
  /* Convert a given Fahrenheit temperature to Celcius */
  convertTempFtoC: function(fTemp) {
    var cTemp;
+   cTemp = ((5/9)*(fTemp - 32));
    return cTemp;
  }
 
